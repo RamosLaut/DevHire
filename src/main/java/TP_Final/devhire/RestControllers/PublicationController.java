d4b17b26d38ef9c2584d5f26ddf4dcb6b135f548
RamosLaut <lautiram0s@hotmail.com>
2025-05-09 20:35:18 -0300

    Controllers creation

---
+++ src/main/java/TP_Final/devhire/RestControllers/PublicationController.java
@@ -0,0 +1,9 @@
+package TP_Final.devhire.RestControllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/publication")
+public class PublicationController {
+}
---
+++ src/main/java/TP_Final/devhire/RestControllers/UserController.java
@@ -0,0 +1,10 @@
+package TP_Final.devhire.RestControllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}
