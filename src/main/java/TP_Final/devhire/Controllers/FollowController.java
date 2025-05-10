66b25d091735bc3f51506e3d08376638479125f8
RamosLaut <lautiram0s@hotmail.com>
2025-05-09 21:02:02 -0300

    Controllers creation

---
+++ src/main/java/TP_Final/devhire/Controllers/FollowController.java
@@ -0,0 +1,9 @@
+package TP_Final.devhire.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/follow")
+public class FollowController {
+}
